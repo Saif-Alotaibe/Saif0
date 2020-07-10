8d7ec47df5813dfa8105ef4aa3326fa298b98133
Saif <safe1otb@gmail.com>
2020-07-10 03:41:04 +0300

    fix bug

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 import datetime
 import os
 db = SQLAlchemy()
@@ -16,7 +15,7 @@ def setup_db(app,database_path=database_path):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
     db.app=app
     db.init_app(app)
-    Migrate(app,db)
+    db.create_all()
     
    # db.create_all()
 '''
